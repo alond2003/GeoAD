a09951a57a84623722bc1f96ed50f208cf8bf674
alond2003 <alond2003@gmail.com>
2021-03-23 17:55:58 +0200

    Remove testy.py from repo

--- beautify.py
+++ beautify.py
@@ -2,9 +2,8 @@
 # TODO: format print points with round(), non float when can, "".join(ps)
 # TODO: format print points from file as h.ps(names,xs,ys)
 # TODO: make all assignments partial until h.calc()
+# TODO: add .gitignore
 # ----------------
 # TODO: make Segment::is_subsegment reverse -> small.is_subsegment_of(large)
-# TODO: from geo import *
 # TODO: add option to not print proof
-# TODO: add .gitignore
 # TODO: add ang bisector method to helper
