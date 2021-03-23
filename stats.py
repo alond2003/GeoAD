bbe394f4ec03d8ca0385219c2692a53354717ffa
alond2003 <alond2003@gmail.com>
2021-03-23 16:11:59 +0200

    Modify .gitignore and stats.py

--- stats.py
+++ stats.py
@@ -1,14 +1,22 @@
-from os import listdir
-
-sumy = 0
-for i in listdir():
-    # print(i)
-    if i[-3:] == ".py":
-        with open(i, "r", encoding="utf8") as f:
-            a = len(f.readlines())
-            print(i, a)
-            sumy += a
+import os
 
 
-print(sumy)
+def stats(path=os.getcwd()):
+    # print("Debug", path)
+    ans = 0
+    for filename in os.listdir(path):
+        if filename.endswith(".py"):
+            with open(os.path.join(path, filename), "r", encoding="utf8") as f:
+                file_len = len(f.readlines())
+                print(os.path.join(path, filename).replace(os.getcwd(), ""), file_len)
+                ans += file_len
+        elif (
+            os.path.isdir(os.path.join(path, filename))
+            and not filename.startswith("__")
+            and not filename.startswith(".")
+        ):
+            ans += stats(os.path.join(path, filename))
+    return ans
 
+
+print(stats())
