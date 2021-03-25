919ae84f3e89b5fdddcdab5b5889173f9dfe13f5
alond2003 <alond2003@gmail.com>
2021-03-25 18:55:24 +0200

    modify .gitignore

--- beautify.py
+++
@@ -1,9 +0,0 @@
-# Done:
-# TODO: format print points with round(), non float when can, "".join(ps)
-# TODO: format print points from file as h.ps(names,xs,ys)
-# TODO: make all assignments partial until h.calc()
-# TODO: add .gitignore
-# ----------------
-# TODO: make Segment::is_subsegment reverse -> small.is_subsegment_of(large)
-# TODO: add option to not print proof
-# TODO: add ang bisector method to helper
